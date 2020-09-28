51f8020e877baa105a280f3ea286d3dbbdb822b0
aashishksahu <krmmvinayak@gmail.com>
2020-09-28 11:00:24 +0530

    Initial commit

---
+++ src/main/java/com/andromeda/SpringWorksApplication.java
@@ -0,0 +1,13 @@
+package com.andromeda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWorksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWorksApplication.class, args);
+	}
+
+}
