ef6d2aa9235525293bf772a7c3844126c0b4b1d0
aashishksahu <krmmvinayak@gmail.com>
2020-09-30 10:27:05 +0530

    ...

--- Spring-Core-DI/src/main/java/com/andromeda/SpringCoreDiApplication.java
+++ Spring-Core-DI/src/main/java/com/andromeda/SpringCoreDiApplication.java
@@ -2,12 +2,18 @@ package com.andromeda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.andromeda.basicDI.Controllers.CarController;
 
 @SpringBootApplication
 public class SpringCoreDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCoreDiApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringCoreDiApplication.class, args);
+
+		System.out.println(ctx.getBean(CarController.class).getCarName());
+
 	}
 
 }
